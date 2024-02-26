fac6407d9ef21e676855bb54f3482934523e96a5
aa860630 <kk860630@gmail.com>
2024-02-27 00:27:19 +0800

    Implement q_remove_head function
    Pop the first entry from the queue.

--- queue.c
+++ queue.c
@@ -62,7 +62,16 @@ bool q_insert_tail(struct list_head *head, char *s)
 /* Remove an element from head of queue */
 element_t *q_remove_head(struct list_head *head, char *sp, size_t bufsize)
 {
-    return NULL;
+    element_t *node = list_first_entry(head, element_t, list);
+
+    list_del_init(head->next);
+
+    if (sp) {
+        strncpy(sp, node->value, bufsize);
+        strcat(sp, "\0");
+    }
+
+    return node;
 }
 
 /* Remove an element from tail of queue */
