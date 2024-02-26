f45007317cd2a9fbade4b84dbb5c78bc3d121c3a
aa860630 <kk860630@gmail.com>
2024-02-27 00:27:36 +0800

    Implement q_remove_tail function
    Pop the last entry from the queue.

--- queue.c
+++ queue.c
@@ -77,7 +77,16 @@ element_t *q_remove_head(struct list_head *head, char *sp, size_t bufsize)
 /* Remove an element from tail of queue */
 element_t *q_remove_tail(struct list_head *head, char *sp, size_t bufsize)
 {
-    return NULL;
+    element_t *node = list_last_entry(head, element_t, list);
+
+    list_del_init(head->prev);
+
+    if (sp) {
+        strncpy(sp, node->value, bufsize);
+        strcat(sp, "\0");
+    }
+
+    return node;
 }
 
 /* Return number of elements in queue */
